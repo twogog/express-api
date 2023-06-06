21a229cc795563767128237ca25dd697bf77bf5c
twogog <twogogtwogog@gmail.com>
2023-06-07 02:14:56 +0600

    test

--- app.js
+++ app.js
@@ -5,31 +5,27 @@ const cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || 3003
-
+const dbPath = path.join(path.resolve(process.cwd(), "public"), "db.json");
 const bodyParser = express.json
 
 app.use(cors())
 
+function getDbData() {
+  return fs.readFileSync(dbPath, "utf8");
+}
+
 app.get('/', (req, res) => {
-  const file = fs.readFileSync(
-    path.join(path.resolve(process.cwd(), "public"), "db.json"),
-    "utf8"
-  );
-  res.json(file)
-  // fs.readFile("./db.json", 'utf-8', (error, data) => {
-  //   console.log(2023)
-  //   res.json(data)
-  // });
+  const db = getDbData()
+  res.json(db)
 })
 
 app.put('/adduser', bodyParser(), (req, res) => {
-  fs.readFile("./db.json", (error, data) => {
-    const response = JSON.parse(data)
-    const {name} = req.body
-    response.users.push({name, score: 0})
-    fs.writeFile("./db.json", JSON.stringify(response), (error, data) => {
-      res.send('success')
-    });
+  const db = getDbData()
+  const {name} = req.body
+  db.users.push({name, score: 0})
+    
+  fs.writeFile(dbPath, JSON.stringify(response), (error, data) => {
+    res.send('success')
   });
 })
 
