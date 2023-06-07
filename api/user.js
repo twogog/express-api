5b130aaf05ca974b19e3f8fe0f589e7b324e6d0d
twogog <you@example.com>
2023-06-08 01:12:24 +0600

    test

--- api/user.js
+++ api/user.js
@@ -1,6 +1,6 @@
 import { kv } from '@vercel/kv';
  
 export default async function handler(request, response) {
-  const user = await kv.hgetall('user:me');
+  const user = await kv.JSON.SET(doc, $, '{"a":2}');
   return response.status(200).json(user);
 }
