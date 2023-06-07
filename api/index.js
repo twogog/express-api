d82259758312d18ecbf8abd9514c4ece877d1eca
twogog <you@example.com>
2023-06-07 20:27:31 +0600

    rebase all logic

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
-const app = require('../app.js');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('sdf')
+})
+
+app.put('/api/adduser', (req, res) => {
+  const {name} = req.body
+ 
+})
+
+app.put('/api/addscore', (req, res) => {
+
+  const {score, name} = req.body
+})
 
 export default app;
