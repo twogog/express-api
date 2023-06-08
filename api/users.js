967d441781470a578df58e423ea61b1a749b21bb
twogog <you@example.com>
2023-06-08 21:26:43 +0600

    done

--- api/users.js
+++ api/users.js
@@ -1,23 +1,32 @@
 import { kv } from '@vercel/kv';
  
 export default async function handler(request, response) {
-  if (!request?.body) {
-    const users = await kv.get('users')
-    return response.status(200).json(users)
-  } else {
-    const {name, score} = request.body;
-    if (!score) {
-      const users = await kv.get('users') || []
-      const newUsers = [...users, { name, score: 0}]
-      const result = await kv.set('users', JSON.stringify(newUsers))
-      return response.status(200).json(result)
-    } else {
-      
-    }
-    
-  }
-  
-  // return response.status(200).json({23: 23});
   // kv.flushall()
   // kv.dbsize()
+  const users = await kv.get('users') || []
+
+  if (!request?.body) return response.status(200).json(users)
+  
+  const {name, score = 0} = request.body;
+
+  if (!score) { // first put
+    const newUsers = users.find(user => user.name === name) 
+      ? users
+      : [...users, { name, score }]
+    newUsers.length !== users.length 
+      ? await kv.set('users', JSON.stringify(newUsers))
+      : '';
+    return response.status(200).json(await kv.get('users'))
+  } else { // not first put
+    const currentUser = users.find(user => user.name === name)
+    if (!currentUser) return response.status(507).json('you have to add an user')
+    if (currentUser.score < score) {
+      const updatedUsers = users.map(user => {
+        return user.name === name ? {...user, score} : user;
+      })
+      await kv.set('users', JSON.stringify(updatedUsers))
+      return response.status(200).json(await kv.get('users'))
+    } else return response.status(200).json(users);
+  }
+  
 }
