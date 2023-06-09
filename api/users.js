b46b71a0059847771c8c228b612eaefb1aad269a
twogog <twogogtwogog@gmail.com>
2023-06-09 17:24:31 +0600

    test

--- api/users.js
+++ api/users.js
@@ -3,9 +3,8 @@ import { kv } from '@vercel/kv';
 export default async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
-  const headersCheck = headers => headers['content-type']?.includes('json')
-  const headers = headersCheck(request.headers);
-  return response.status(200).json(request)
+  const headersCheck = req => req.body && req.headers['content-type']?.includes('json')
+  return response.status(200).json(headersCheck(request))
   if (request.body && !headersCheck(request.headers)) return response.status(400).json('content-type should be a json type')
   const users = await kv.get('users') || []
 
