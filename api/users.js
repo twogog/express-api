b8361b939abf743974fcc7cd7b10e82eed468764
twogog <twogogtwogog@gmail.com>
2023-06-12 22:38:11 +0600

    fixed registr logic

--- api/users.js
+++ api/users.js
@@ -4,6 +4,6 @@ export default async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
   const users = await kv.get('users') || []
-  const toFront = users.map(({name, score}) => {name, score})
+  const toFront = users.map(({name, score}) => ({name, score}))
   return response.status(200).json(toFront)
 }
