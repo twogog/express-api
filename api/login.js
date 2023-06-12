2cc3bd3761a69db196311c7c69cb60d2848fa125
twogog <twogogtwogog@gmail.com>
2023-06-12 21:23:58 +0600

    added login, registration, users routes

--- api/login.js
+++ api/login.js
@@ -5,11 +5,7 @@ export default async function handler(request, response) {
   // kv.dbsize()
   const {name, password} = request.body;
   const registratedUsers = await kv.get('users') || []
-  const toFront = registratedUsers.map(({name, score}) => {name, score})
   const currentUser = registratedUsers.find(user => user.name === name);
   if (currentUser.password === password) return response.status(200).json('login');
   return response.status(400).json('incorrect pass or nickname')
-  
-  await kv.set('users', JSON.stringify(...registratedUsers, newUser))
-  return response.status(200).json('success');
 }
--- api/users.js
+++ api/users.js
@@ -3,31 +3,7 @@ import { kv } from '@vercel/kv';
 export default async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
-  
-  const users = await kv.get('secret') || []
-
-  if (!request?.body) return response.status(200).json(users)
-  
-  const {name, score = 0} = request.body;
-  if(!name) return response.status(400).json('something wrong with your body or content-type')
-  if (!score) { // first put
-    const newUsers = users.find(user => user.name === name) 
-      ? users
-      : [...users, { name, score }]
-    newUsers.length !== users.length 
-      ? await kv.set('users', JSON.stringify(newUsers))
-      : '';
-    return response.status(200).json(await kv.get('users'))
-  } else { // not first put
-    const currentUser = users.find(user => user.name === name)
-    if (!currentUser) return response.status(400).json('you have to add an user')
-    if (currentUser.score < score) {
-      const updatedUsers = users.map(user => {
-        return user.name === name ? {...user, score} : user;
-      })
-      await kv.set('users', JSON.stringify(updatedUsers))
-      return response.status(200).json(await kv.get('users'))
-    } else return response.status(200).json(users);
-  }
-  
+  const users = await kv.get('users') || []
+  const toFront = users.map(({name, score}) => {name, score})
+  return response.status(200).json(toFront)
 }
