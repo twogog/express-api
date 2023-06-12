c1073c07e5e6c85ac7ea3de6e6ccb585ad96ed3c
twogog <twogogtwogog@gmail.com>
2023-06-13 00:30:24 +0600

    test

--- api/registration.js
+++ api/registration.js
@@ -3,7 +3,7 @@ import { kv } from '@vercel/kv';
 export default async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
-  const {email, name, password, score = 0} = JSON.parse(request.body);
+  const {email, name, password, score = 0} = request.body;
   const registratedUsers = await kv.get('users') || []
   const currentUser = registratedUsers.find(user => user.name === name);
   if (currentUser) return response.status(400).json('user already exist');
