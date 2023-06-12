4d0abbb18e7d5ebba27fa271963c26ca94de25bd
twogog <twogogtwogog@gmail.com>
2023-06-13 01:43:56 +0600

    test

--- api/registration.js
+++ api/registration.js
@@ -3,7 +3,6 @@ import { kv } from '@vercel/kv';
 export default async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
-  try {
     const {email, name, password, score = 0} = request.body;
     const registratedUsers = await kv.get('users') || []
     const currentUser = registratedUsers.find(user => user.name === name);
@@ -12,8 +11,4 @@ export default async function handler(request, response) {
     
     await kv.set('users', JSON.stringify([...registratedUsers, newUser]))
     return response.status(200).json('success');  
-  } catch (error) {
-    return response.status(400).json(error.message)   
-  }
-  
 }
