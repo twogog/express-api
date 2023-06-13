ce5386ac71f580376e6085565c140638600f33b0
twogog <twogogtwogog@gmail.com>
2023-06-13 21:52:13 +0600

    test

--- api/login.js
+++ api/login.js
@@ -1,6 +1,7 @@
 import { kv } from '@vercel/kv';
- 
-export default async function handler(request, response) {
+import { allowCors } from '../cors';
+
+async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
   const {name, password} = request.body;
@@ -9,3 +10,5 @@ export default async function handler(request, response) {
   if (currentUser.password === password) return response.status(200).json('login');
   return response.status(400).json('incorrect pass or nickname')
 }
+
+module.exports = allowCors(handler)
