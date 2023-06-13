2c1cc1c73e8ea6c3f760195f138aad3c58697d4e
twogog <twogogtwogog@gmail.com>
2023-06-13 22:19:59 +0600

    still testing cors...

--- api/score.js
+++ api/score.js
@@ -1,11 +1,25 @@
 import { kv } from '@vercel/kv';
- 
-export default async function handler(request, response) {
+import { allowCors } from '../cors';
+
+async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
   const {name, score} = request.body;
   const registratedUsers = await kv.get('users') || []
   const currentUser = registratedUsers.find(user => user.name === name);
-  currentUser.score = currentUser.score < score ? score : currentUser.score
-  await kv.set('users', JSON.stringify())
+  if (currentUser.score < score) {
+    const updatedUsers = registratedUsers.map(user => {
+      return user.name === name 
+        ?  {...user, score}
+        : user
+    })
+    await kv.set('users', JSON.stringify(updatedUsers))
+    const result = await kv.get('users') || []
+    const toFront = result.map(({name, score}) => ({name, score}))
+    return response.status(200).json(toFront)
+  }
+  const toFront = registratedUsers.map(({name, score}) => ({name, score}))
+  return response.status(200).json(toFront)
 }
+
+module.exports = allowCors(handler)
