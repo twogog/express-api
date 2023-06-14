5b8c46d10c5280dbddf874846ad124c1be690f09
twogog <twogogtwogog@gmail.com>
2023-06-14 13:27:33 +0600

    some guards

--- api/login.js
+++ api/login.js
@@ -7,8 +7,8 @@ async function handler(request, response) {
   const {name, password} = request.body;
   const registratedUsers = await kv.get('users') || []
   const currentUser = registratedUsers.find(user => user.name === name);
-  if (currentUser.password === password) return response.status(200).json('login');
-  return response.status(400).json('incorrect pass or nickname')
+  if (password && currentUser?.password === password) return response.status(200).json('login');
+  return response.status(400).json('incorrect password or nickname')
 }
 
 module.exports = allowCors(handler)
--- api/registration.js
+++ api/registration.js
@@ -5,11 +5,11 @@ async function handler(request, response) {
   // kv.flushall()
   // kv.dbsize()
     const {email, name, password, score = 0} = request.body;
+    if (!email || !name || !password) return response.status(400).json('request is incorrect')
     const registratedUsers = await kv.get('users') || []
     const currentUser = registratedUsers.find(user => user.name === name);
     if (currentUser) return response.status(400).json('user already exist');
     const newUser = {email, name, password, score}
-    
     await kv.set('users', JSON.stringify([...registratedUsers, newUser]))
     return response.status(200).json('success');  
 }
