25cc3dc4b6ab9b2b82e88d91a9ecc0d340619bbb
twogog <you@example.com>
2023-06-14 23:45:11 +0600

    fixing auth logic

--- api/login.js
+++ api/login.js
@@ -6,8 +6,9 @@ async function handler(request, response) {
   // kv.dbsize()
   const {name, password} = request.body;
   const registratedUsers = await kv.get('users') || []
+  const toFront = registratedUsers.map(({name, score}) => ({name, score}))
   const currentUser = registratedUsers.find(user => user.name === name);
-  if (password && currentUser?.password === password) return response.status(200).json('login');
+  if (password && currentUser?.password === password) return response.status(200).json(toFront);
   return response.status(400).json('incorrect password or nickname')
 }
 
--- api/registration.js
+++ api/registration.js
@@ -10,8 +10,9 @@ async function handler(request, response) {
     const currentUser = registratedUsers.find(user => user.name === name);
     if (currentUser) return response.status(400).json('user already exist');
     const newUser = {email, name, password, score}
-    await kv.set('users', JSON.stringify([...registratedUsers, newUser]))
-    return response.status(200).json('success');  
+    const result = await kv.set('users', JSON.stringify([...registratedUsers, newUser]))
+    const toFront = result.map(({name, score}) => ({name, score}))
+    return response.status(200).json(toFront);
 }
 
 module.exports = allowCors(handler)
