e8676c0916c7256be5451ff3b283d187e85c02ff
twogog <you@example.com>
2023-06-14 23:49:35 +0600

    fix

--- api/registration.js
+++ api/registration.js
@@ -10,8 +10,8 @@ async function handler(request, response) {
     const currentUser = registratedUsers.find(user => user.name === name);
     if (currentUser) return response.status(400).json('user already exist');
     const newUser = {email, name, password, score}
-    const result = await kv.set('users', JSON.stringify([...registratedUsers, newUser]))
-    const toFront = result.map(({name, score}) => ({name, score}))
+    await kv.set('users', JSON.stringify([...registratedUsers, newUser]))
+    const toFront = (await kv.get('users')).map(({name, score}) => ({name, score}))
     return response.status(200).json(toFront);
 }
 
